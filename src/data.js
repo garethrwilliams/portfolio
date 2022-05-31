baeb46d15e6cbf3d85d41057bd5bcb2e48ed22d8
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-05-31 14:33:47 +0100

    creates portfolio

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    title: 'News API',
+    subtitle: 'Express Router // PSQL',
+    description:
+      'This API is for the purpose of accessing application data programmatically, the API creates a real world backend service which has the functionality to pass information to frontend architecture',
+    image: './backend.png',
+    link: 'https://github.com/garethrwilliams/NC-News',
+  },
+  {
+    title: 'React News Aggregator',
+    subtitle: 'React // Tailwind',
+    description:
+      'NC News is a reddit style react web app where users can post, read, and comment upon articles. Articles can be filtered by category, and sorted with a variety of metrics.',
+    image: './frontend.png',
+    link: 'https://reactbootcamp.com',
+  },
+  {
+    title: 'Murdle',
+    subtitle: 'React Native // Firebase',
+    description:
+      'A stylized wordle type game with added functionality and features, The app was built as a mobile app and has links to a db in order to record and collate scores',
+    image: './murdle.png',
+    link: 'https://github.com/garethrwilliams/maximal-murdle',
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+    image: 'https://randomuser.me/api/portraits/men/1.jpg',
+    name: 'Jesse Hicks',
+    company: 'Zoozle',
+  },
+  {
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+    image: 'https://randomuser.me/api/portraits/men/94.jpg',
+    name: 'Ruben Alvarez',
+    company: 'Dooble.io',
+  },
+];
+
+export const skills = [
+  'JavaScript',
+  'Python',
+  'Express.js',
+  'SQL',
+  'React',
+  'React Native',
+  'Firebase',
+  'Node',
+  'GraphQL',
+  'Tailwind',
+  'HTML5',
+  'CSS',
+  'Jest',
+];
