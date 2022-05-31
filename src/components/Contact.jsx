fae1bb3077007effbca8158559861c0df8a2d1d5
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-05-31 14:42:09 +0100

    fixes error

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -58,9 +58,9 @@ export default function Contact() {
               <h2 className='title-font font-semibold text-white tracking-widest text-xs'>
                 EMAIL
               </h2>
-              <a className='text-indigo-400 leading-relaxed'>
+              <h2 className='text-indigo-400 leading-relaxed'>
                 garethrwilliams@me.com
-              </a>
+              </h2>
               <h2 className='title-font font-semibold text-white tracking-widest text-xs mt-4'>
                 PHONE
               </h2>
