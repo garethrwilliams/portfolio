542f884c8e186844a62d1500f35d0f7fcefca9c5
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-06-18 13:17:08 +0100

    Moves initialisation within app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import ReactGA from 'react-ga';
-
-// tracking id from Google
-const TRACKING_ID = 'UA-232344407-2';
-ReactGA.initialize(TRACKING_ID);
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    // tracking id from Google
+    const TRACKING_ID = 'UA-232344407-2';
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
   return (
     <main className='text-gray-400 bg-gray-900 body-font'>
       <Navbar />
